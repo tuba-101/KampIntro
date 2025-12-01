5da4ac71ab267d7ba3a01014dd59b4381505b5a9
TNKORKMAZ <TUGBANUR.KORKMAZ@iett.gov.tr>
2025-12-01 15:57:59 +0300

    4. video eklediklerim

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Http.Headers;
+
+namespace Koleksiyonlar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> isimler2 = new List<string>() { "engin", "murat", "kerem", "halil" };
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+            isimler2.Add(isimler2[4]);
+            Console.WriteLine(isimler2[4]);
+
+        }
+    }
+}
