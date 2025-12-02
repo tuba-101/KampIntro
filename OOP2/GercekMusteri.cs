fdc6385a223f1772ef44851a7500c5989c2a9238
TNKORKMAZ <TUGBANUR.KORKMAZ@iett.gov.tr>
2025-12-02 16:34:25 +0300

    5.ders değişiklikler

---
+++ OOP2/GercekMusteri.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP2
+{
+    //inheritance
+    internal class GercekMusteri:Musteri
+    {
+ 
+        public string Adi { get; set; }
+        public string Soyadi { get; set; }
+        public string TCNo { get; set; }
+    }
+}
---
+++ OOP2/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace OOP2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.Adi = "engin";
+            musteri1.Soyadi = "demiroğ";
+            musteri1.TCNo = "22222222222";
+            musteri1.MusteriNo = "12345";
+            musteri1.Id = 1;
+
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "54321";
+            musteri2.SirketAdi = "Kodlama.io";
+            musteri2.VergiNo = "123456789";
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+            //inheritance kullandığımız için hem tüzel kişiyi hem de gerçek kişiyi customer manager a parametre olarak verebiliyoruz.
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(musteri1);
+            customerManager.Add(musteri2);
+
+
+
+        }
+    }
+}
